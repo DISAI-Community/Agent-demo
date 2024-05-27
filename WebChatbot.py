c0f5558b37f096ac4727c791c6b21d45e29e5eda
raaasin <nisarvskp@gmail.com>
2024-05-27 17:02:45 +0530

    chat history included, improved context

--- WebChatbot.py
+++ WebChatbot.py
@@ -8,18 +8,20 @@ load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
 #Define agent properties
 expertise = "Webagent"
-task = Task("Search web and answer accordingly")
+task = Task("Search web and answer accordingly only if required.")
 input_type = InputType("Text")
 output_type = OutputType("Text")
 agent = Agent(expertise, task, input_type, output_type)
 api_key = openai_api_key
-model = OpenAIModel(api_key=api_key,model="gpt-3.5-turbo")
-sequential_flow = SequentialFlow(agent, model)
+chat_history=[]
+model = OpenAIModel(api_key=api_key,model="gpt-3.5-turbo",chat_history=chat_history)
+
+sequential_flow = SequentialFlow(agent, model, )
 
 st.title("💬 DISAI Webagent ")
 st.caption("🚀 A streamlit chat agent powered by DSAI Framework")
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "Hi I'm a webagent, I am an LLM connected to the internet, ask me! 😊"}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "Hi I am your Llm web agent connected to internet. AMA! 😊"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
@@ -30,3 +32,4 @@ if prompt := st.chat_input():
     msg= sequential_flow.web_agent(prompt)
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
+    #print(chat_history)
